aabb6f03ea683e4a4bba1bb0dbba916ab4c5a0c2
havenquan <havenquan@qq.com>
2021-11-17 18:41:10 +0800

    apr

--- src/info.ts
+++ src/info.ts
@@ -1,13 +1,15 @@
 import { hc } from './token/HC';
 import { hnPool } from './pool/HNPool';
+import { hnBox } from './pool/HNBox';
 import { contract } from './constant';
 
 const maxLevel = 5;
-const cardPrice = 110;
-const usdPerLevel = [cardPrice * 1, cardPrice * 4, cardPrice * 16, cardPrice * 64, cardPrice * 256];
+const costPerLevel = [1, 4, 16, 64, 256];
 
 export const info = {
   getHNPoolApr: async (hcPrice: number, btcPrice: number) => {
+    const cardPrice = Number(await hnBox().boxTokenPrices(1)) / 1e18;
+
     const hcPerYear = Number((await hc().getPoolTokenPerBlock(contract().HNPool)).mul(28800 * 365)) / 1e18;
     const hcValuePerYear = hcPerYear * hcPrice;
 
@@ -18,13 +20,15 @@ export const info = {
 
     let stakeUsdValue = 0;
     for (let i = 0; i < maxLevel; i++) {
-      stakeUsdValue += hnIdsLengthPerLevel[i] * usdPerLevel[i];
+      stakeUsdValue += hnIdsLengthPerLevel[i] * costPerLevel[i] * cardPrice;
     }
 
     return (hcValuePerYear + btcValuePerYear) / stakeUsdValue * 100;
   },
 
   getHNPoolUserApr: async (user: string, hcPrice: number, btcPrice: number) => {
+    const cardPrice = Number(await hnBox().boxTokenPrices(1)) / 1e18;
+
     const stakeHc = Number((await hnPool().stakes(0)));
     const userStakeHc = Number((await hnPool().userStakes(user, 0)));
     const userShareHc = userStakeHc / stakeHc;
@@ -45,7 +49,7 @@ export const info = {
 
     let userStakeUsdValue = 0;
     for (let i = 0; i < maxLevel; i++) {
-      userStakeUsdValue += userHnIdsLengthPerLevel[i] * usdPerLevel[i];
+      userStakeUsdValue += userHnIdsLengthPerLevel[i] * costPerLevel[i] * cardPrice;
     }
 
     return (userHcValuePerYear + userBtcValuePerYear) / userStakeUsdValue * 100;
