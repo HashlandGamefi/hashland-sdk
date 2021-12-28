a8e398dadeac63ea61a387c4656f73ca94c17548
havenquan <havenquan@qq.com>
2021-12-28 18:24:25 +0800

    hwdepositinfo

--- src/tool/HWDeposit.ts
+++ src/tool/HWDeposit.ts
@@ -1,7 +1,47 @@
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client'
 import { HWDeposit__factory } from 'hashland-core/typechain-types'
 import { rpcProvider } from '../utils';
 import { contract } from '../constant';
 
+const client = new ApolloClient({
+  uri: 'https://api.thegraph.com/subgraphs/name/hashlandgamefi/hw-deposit',
+  cache: new InMemoryCache(),
+})
+
 export function hwDeposit() {
   return HWDeposit__factory.connect(contract().HWDeposit, rpcProvider);
+}
+
+export const hwDepositInfo = {
+  getUserInfo: async (
+    first: number,
+    skip: number,
+    orderBy: string,
+    orderDirection: string,
+    user?: string,
+  ) => {
+    const userInfoQuery = `
+      query($first: Int, $skip: Int, $orderBy: BigInt, $orderDirection: String, $user: String) {
+        userInfos(
+          first: $first, skip: $skip, orderBy: $orderBy, orderDirection: $orderDirection,
+          where: {${user ? `user: $user,` : ``}}
+        ) {
+          user
+          amount
+          count
+        }
+      }
+    `;
+
+    return await client.query({
+      query: gql(userInfoQuery),
+      variables: {
+        first: first,
+        skip: skip,
+        orderBy: orderBy,
+        orderDirection: orderDirection,
+        user: user,
+      },
+    });
+  },
 }
